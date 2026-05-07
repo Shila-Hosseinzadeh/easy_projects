38b8bbd1b0481102dd8fc71554f0b2d18d552696
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-05-07 13:25:20 +0330

    Create Loop Sets.py

---
+++ Loop Sets.py
@@ -0,0 +1,9 @@
+# Loop Sets : in sets only have for not while
+# for
+myset = {"a", "b", "c", "d","e","f" }
+print(f"myset: {myset}")
+for i in myset:
+    print(f" myset items are: {i}") # \
+    # items not in order. we have different order in every for.\
+    # because in every run we have a different my set. \
+    # for chooses every loop in every different myset
