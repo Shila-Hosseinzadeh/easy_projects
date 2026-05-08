5c07d0dd498625bf6a4856005c5b824a8d7194bf
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-05-09 00:46:59 +0330

    Update Join Sets.py

--- Join Sets.py
+++ Join Sets.py
@@ -1,1 +1,14 @@
 # Join Sets
+# union()
+set1 = {1,2,3,4,5}
+set2 = {"a","b","c",1,3}
+set11 = {1,2,3,4,5,33,"G","d"}
+set12 = {"a","b","c",1,3,"E",22,45}
+set3 = set1.union(set2,set11,set12)
+print(f"set1.union(set2,set11,set12,...) ---> set3 ={set3}")
+
+# |
+set4 = set1 | set1 |set11 |set12
+print(f"set1 | set2 |se11 |set12 ---> set4 = {set4}")
+
+#
