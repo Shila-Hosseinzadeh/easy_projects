39dc2e886e5f0eb49c13dcb6b7a65b9579f9a0b5
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-05-10 00:49:45 +0330

    Update frozen_set.py

--- frozen_set.py
+++ frozen_set.py
@@ -6,16 +6,34 @@ print(s1)
 s2 = s1.copy()
 print(s2)
 
-# union() of frozensets === | or operator 
+# union() of frozensets === | or : operator 
 s1 = frozenset {1,2,3,4}
-s2 ={1,2,3,5,7,8}
+s2 =frozenset{1,2,3,5,7,8}
 s3 = s1 .union(s2)
 print(s3)
 
-# intersection() of frozensets === & and operator 
+# intersection() of frozensets === & : and operator 
 s1 = frozenset {1,2,3,4}
-s2 ={1,2,3,5,7,8}
-s3 = s1 .union(s2)
+s2 =frozenset{1,2,3,5,7,8}
+s3 = s1 .intersection(s2)
 print(s3)
+
+# difference() of frozensets === s1 - s2 === members of only s1
+s1 = frozenset {1,2,3,4}
+s2 =frozenset{1,2,3,5,7,8}
+s3 = s1 .difference(s2)
+print(s3)
+
+# symmetric_difference() of frozensets === s1 ^ s2 === non-shared members of all sets
+s1 = frozenset {1,2,3,4}
+s2 =frozenset{1,2,3,5,7,8}
+s3 = s1 .symmetric_difference(s2)
+print(s3)
+
+# subset() , superset()
+s1 =frozenset{1,2,3,4,5,6}
+s2 =frozenset {2,3,5}
+print(s1.issuperset(s2))
+print(s2.issubset(s1))
 
 
