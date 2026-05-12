e3e10530dcdbf19f2c88daee7109b27b3e8b0f2e
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-05-12 21:33:32 +0330

    Create Set Methods.py

---
+++ Set Methods.py
@@ -0,0 +1,25 @@
+# add() , clear() , copy
+s1 = {"a","b" ,"c"}
+s2 = s1.copy()
+s2.add("d")
+print(s1)
+print (s2)
+# sets are not in order and members are unique
+
+# difference() difference_update() , - , =-
+
+a={1,2,3,4,5}
+b={1,2,4,5,6,7}
+only_a = a.difference(b)
+print(only_a)
+a.difference_update(b)
+print(a)
+
+# remove() with error ,without error discard()
+
+# intersection() , intersection_update() , & , =&
+# issubset() ,issuperset() , isdisjoint()
+# pop() delet member random from sets ,remove() delets decided member from sets
+# symmetric_difference() ,symmetric_difference_update() ,^ ,=^
+# update() ,union() , | , =|
+# 
