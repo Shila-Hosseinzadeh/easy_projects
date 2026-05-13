ccb9385db5e0ffc89fccce6d543c785856746f2d
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-05-14 00:33:27 +0330

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,15 @@
+#set of keys and values {key:value} , is ordered , doesn't have duplicate member
 
+dic1 ={"a":1,"b":2 ,"c":3}
+print(dic1[a]) #prints values of keys
+print(type(dic1))
+print(len(dic1))
+# dic() method make dictionary
+# access items
+print(dic1["a"]) # if there isn't the key we have error
+# get() if there is not key we don't have error, we have None
+print(dic1.get("c"))
+#keys() method is refference type
+print(dic1.keys())
+dic1["a"]=5
+print(dic1.keys())
